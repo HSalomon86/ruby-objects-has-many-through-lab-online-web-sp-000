c8c22128136ff74aaf50518a9453be522dde6ead
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-25 02:26:45 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,4 +8,23 @@ class Doctor
     @@all
   end
 
+  def new_appointment(patient, date)
+    new_a = Appointment.new
+
+  end
+
+  def appointments
+    Appointment.all.select do |app|
+      app.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |app|
+      app.patient
+    end
+  end
+
+  end
+
 end
